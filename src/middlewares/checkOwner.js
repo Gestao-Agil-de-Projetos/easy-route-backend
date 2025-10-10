c411d6bd93e20a5ef36d893b9369cbec720919ac
CauaCurvelo <cauacrmeira@gmail.com>
2025-10-09 22:06:57 -0300

    Merge: Integra alterações remotas da branch feature/9-criar-crud-vans

--- src/middlewares/checkOwner.js
+++
@@ -1,9 +0,0 @@
-
-export async function checkOwner(request, reply) {
-
-  const userRole = request.user.role;
-
-  if (userRole !== 'OWNER') {
-    return reply.code(403).send({ message: 'Acesso negado. Esta ação é permitida apenas para usuários do tipo OWNER.' });
-  }
-}
