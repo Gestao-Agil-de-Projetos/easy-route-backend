b21adb9140e902fe8250685a8177d540d0876e54
CauaCurvelo <cauacrmeira@gmail.com>
2025-10-10 02:13:29 -0300

    chore: revert ports to 3000 and update env default port

--- src/utils/envs.js
+++ src/utils/envs.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const isDev = () => process.env.NODE_ENV === 'development'
 
 export const envs = {
-  PORT: Number(process.env.PORT) || 7000,
+  PORT: Number(process.env.PORT) || 3000,
   HOST: process.env.HOST || 'localhost',
   JWT_SECRET: process.env.JWT_SECRET,
 }
